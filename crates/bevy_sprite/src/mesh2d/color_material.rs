bd96f5a3fd764738e421a8f21bb611536e881a22
Brian Reavis <brian@thirdroute.com>
2024-05-20 00:15:05 -0700

    Derive PartialEq for ColorMaterial

--- crates/bevy_sprite/src/mesh2d/color_material.rs
+++ crates/bevy_sprite/src/mesh2d/color_material.rs
@@ -39,7 +39,7 @@ impl Plugin for ColorMaterialPlugin {
 }
 
 /// A [2d material](Material2d) that renders [2d meshes](crate::Mesh2d) with a texture tinted by a uniform color
-#[derive(Asset, AsBindGroup, Reflect, Debug, Clone)]
+#[derive(Asset, AsBindGroup, Reflect, Debug, Clone, PartialEq)]
 #[reflect(Default, Debug)]
 #[uniform(0, ColorMaterialUniform)]
 pub struct ColorMaterial {
